adc3050cf6996061eed05f37c015dd4e52a8acd6
Amanjyoti Mridha <quackingbob101@gmail.com>
2021-12-31 19:45:38 -0800

    Project Model

--- EnvironmentML/main/models.py
+++ EnvironmentML/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    project_date = models.DateTimeField('Date Completed')
+    project_summary = models.TextField()
+    project_article = models.TextField()
+    project_image = models.ImageField()
+
+    def __str__(self):
+        return self.project_name
