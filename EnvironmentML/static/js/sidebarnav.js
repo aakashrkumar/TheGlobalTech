1d2acd76d8bd21e41a5d41ad1b199a32c7cf32c7
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-02 09:32:59 -0800

    Update

--- EnvironmentML/static/js/sidebarnav.js
+++ EnvironmentML/static/js/sidebarnav.js
@@ -6,3 +6,13 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
 }
+
+jQuery('body').bind('click', function (e) {
+    if (jQuery(e.target).closest('.navbar').length == 0) {
+        // click happened outside of .navbar, so hide
+        var opened = jQuery('.navbar-collapse').hasClass('collapse in');
+        if (opened === true) {
+            jQuery('.navbar-collapse').collapse('hide');
+        }
+    }
+});
