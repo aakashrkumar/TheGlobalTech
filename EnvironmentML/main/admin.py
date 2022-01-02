b8e18af0978f7e9897b62ac2c75a05c443a2c715
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-01 19:07:05 -0800

    Update

--- EnvironmentML/main/admin.py
+++ EnvironmentML/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Project
+from tinymce.widgets import TinyMCE
+from django.db import models
 
-# Register your models here.
+
+# Register your models here
+class ProjectAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+    }
+
+
+admin.site.register(Project, ProjectAdmin)
