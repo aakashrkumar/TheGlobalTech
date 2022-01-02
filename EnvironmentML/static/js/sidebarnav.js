b2fa0795e67a768943f7e46bd2184bd4af59cc5d
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-02 09:41:20 -0800

    Update

--- EnvironmentML/static/js/sidebarnav.js
+++ EnvironmentML/static/js/sidebarnav.js
@@ -1,17 +1,17 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("sideNavBar").style.width = "250px";
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("sideNavBar").style.width = "0";
 }
 
 jQuery('body').bind('click', function (e) {
     if (jQuery(e.target).closest('.sidebarnav').length == 0) {
         console.log("click")
         // click happened outside of .navbar, so hide
-        var opened = jQuery('.sidebarnav').style.width == "0";
+        var opened = jQuery('.sideNavBar').style.width == "0";
         if (opened === true) {
             closeNav();
         }
