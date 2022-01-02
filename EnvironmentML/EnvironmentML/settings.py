cb34a59e55592e492c64308c095884d0997194dc
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-02 13:06:32 -0800

    Update

--- EnvironmentML/EnvironmentML/settings.py
+++ EnvironmentML/EnvironmentML/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-MEDIA_ROOT = "main/"
+MEDIA_ROOT = "media/"
 STATICFILES_DIRS = []
