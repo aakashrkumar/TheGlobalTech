3304ab16918626dee50d64ef14371385f4f8987c
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-02 09:45:13 -0800

    Update

--- EnvironmentML/static/js/sidebarnav.js
+++ EnvironmentML/static/js/sidebarnav.js
@@ -9,7 +9,7 @@ function closeNav() {
 
 jQuery('body').bind('click', function (e) {
     if (jQuery(e.target).closest('.sidebarnav').length == 0) {
-        console.log(jQuery('.sidebarnav').style);
+        console.log(jQuery('.sidebarnav'));
         // click happened outside of .navbar, so hide
         var opened = jQuery('.sidebarnav').style == "0";
         if (opened === true) {
