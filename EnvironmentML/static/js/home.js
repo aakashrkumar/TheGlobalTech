b75601835b27c975df5a283ddae79e5f01bd6c7f
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-02 17:59:27 -0800

    Update

--- EnvironmentML/static/js/home.js
+++ EnvironmentML/static/js/home.js
@@ -24,5 +24,4 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace("activeimg", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
