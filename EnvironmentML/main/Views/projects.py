8e481a3e9d62ee76ed83c5ddb0dff01c6edb760d
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-02 18:17:41 -0800

    Update

--- EnvironmentML/main/Views/projects.py
+++ EnvironmentML/main/Views/projects.py
@@ -6,4 +6,4 @@ def projects(request):
     projectsList = list(Project.objects.all())
     for i in range(30):
         projectsList.append(projectsList[0])
-    return render(request=request, template_name="main/projectsList.html", context={"projectsList": projectsList})
+    return render(request=request, template_name="main/projects.html", context={"projects": projectsList})
