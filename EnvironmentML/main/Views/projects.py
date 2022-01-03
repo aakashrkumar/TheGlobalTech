f726335aca3af45d0984d39e6ed12b6cc7b782bf
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-02 16:27:13 -0800

    Update

--- EnvironmentML/main/Views/projects.py
+++ EnvironmentML/main/Views/projects.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
+from ..models import Project
 
 
 def projects(request):
-    return render(request=request, template_name="main/projects.html")
+    return render(request=request, template_name="main/projects.html", context={"projects": Project.objects.all()})
