d07a09bf556eff210a0e31455b8fcb36c2168c1c
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-02 18:07:46 -0800

    Update

--- EnvironmentML/EnvironmentML/settings.py
+++ EnvironmentML/EnvironmentML/settings.py
@@ -23,7 +23,6 @@ SECRET_KEY = 'django-insecure-e*1$oedz8$s*!7!0h+_5c2sb)2@a!j2wnn4n617-=92heo5^gg
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-APPEND_SLASH = True
 
 # CHANGE THIS
 ALLOWED_HOSTS = ["*"]
