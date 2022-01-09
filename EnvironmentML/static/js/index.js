5b053fd43bef226752781b0cdd92f9e666c8d959
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-09 11:01:41 -0800

    Update

--- EnvironmentML/static/js/index.js
+++ EnvironmentML/static/js/index.js
@@ -39,3 +39,14 @@ window.addEventListener('pageshow', function (event) {
     var fader = document.getElementById('fader');
     fader.classList.remove('fade-in');
 });
+
+function scrollbarWidth() {
+    var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div>');
+    // Append our div, do our calculation and then remove it
+    $('body').append(div);
+    var w1 = $('div', div).innerWidth();
+    div.css('overflow-y', 'scroll');
+    var w2 = $('div', div).innerWidth();
+    $(div).remove();
+    return (w1 - w2);
+}
