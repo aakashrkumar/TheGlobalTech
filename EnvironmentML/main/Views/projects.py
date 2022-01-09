550a36ae7ddd399e6f37e9f32aec2f0509578c81
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 18:16:12 -0800

    Update

--- EnvironmentML/main/Views/projects.py
+++ EnvironmentML/main/Views/projects.py
@@ -15,9 +15,12 @@ def projects(request):
     return render(request=request, template_name="main/projects.html", context={"projects": projectsList})
 
 
-def project():
-    pass
+def projectRender(request, project):
+    return render(request=request, template_name="main/project.html", context={"project": project})
 
 
 def slug(request, slug):
-    return HttpResponse(f"<h1>{slug}</h1>")
+    projectsList = list(Project.objects.all().order_by('-project_date').filter(project_slug=slug))
+    if len(projectsList) > 0:
+        return projectRender(request, projectsList[0])
+    return HttpResponse(status=404)
