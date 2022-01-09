0073f93683fc81d1383a3cbc2d6a9556131efaef
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-09 09:57:43 -0800

    Update

--- EnvironmentML/main/Views/aboutus.py
+++ EnvironmentML/main/Views/aboutus.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 
 def aboutus(request):
     team = User.objects.all().filter(is_staff=True)
-    return render(request, 'aboutus.html', context={'team': team})
+    return render(request, 'main/aboutus.html', context={'team': team})
