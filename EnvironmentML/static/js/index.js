a07e2b6a87a6dfa323afc92063384200f2b5e296
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 19:10:24 -0800

    Update

--- EnvironmentML/static/js/index.js
+++ EnvironmentML/static/js/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('.main-content').css('display', 'none');
-  $('.main-content').fadeIn(1000);
+  $('.main-content').fadeIn(2000);
   $('.link').click(function(event) {
     event.preventDefault();
     newLocation = $('.link a').attr("href");
-    $('.main-content').fadeOut(1000, newpage);
+    $('.main-content').fadeOut(2000, newpage);
   });
   function newpage() {
     window.location = newLocation;
