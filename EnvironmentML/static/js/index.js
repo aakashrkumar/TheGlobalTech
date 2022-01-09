04ce23b416df59122f936f14c4667b4ebd92c39b
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 18:36:06 -0800

    Update

--- EnvironmentML/static/js/index.js
+++ EnvironmentML/static/js/index.js
@@ -9,37 +9,3 @@ function fadeInPage() {
     var fader = document.getElementById('fader');
     fader.classList.add('fade-out');
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-    if (!window.AnimationEvent) {
-        return;
-    }
-    var anchors = document.getElementsByTagName('a');
-
-    for (var idx = 0; idx < anchors.length; idx += 1) {
-        if (anchors[idx].hostname !== window.location.hostname ||
-            anchors[idx].pathname === window.location.pathname) {
-            continue;
-        }
-        anchors[idx].addEventListener('click', function (event) {
-            var fader = document.getElementById('fader'),
-                anchor = event.currentTarget;
-
-            var listener = function () {
-                window.location = anchor.href;
-                fader.removeEventListener('animationend', listener);
-            };
-            fader.addEventListener('animationend', listener);
-
-            event.preventDefault();
-            fader.setAttribute('class', 'fade-in');
-        });
-    }
-});
-window.addEventListener('pageshow', function (event) {
-    if (!event.persisted) {
-        return;
-    }
-    var fader = document.getElementById('fader');
-    fader.setAttribute('class', 'fade-out');
-});
