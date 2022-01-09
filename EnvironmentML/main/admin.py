4331a90d4a530fbb24642546366e557398bf8ece
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 20:16:39 -0800

    Update

--- EnvironmentML/main/admin.py
+++ EnvironmentML/main/admin.py
@@ -20,7 +20,9 @@ class ProjectAdmin(admin.ModelAdmin):
         models.TextField: {'widget': TinyMCE()},
         models.ManyToManyField: {'widget': admin.widgets.FilteredSelectMultiple('authors', False)},
     }
-    list_display = (formfield_for_manytomany,)
+    # set "project_authors" to use formfield_for_manytomany
+    inlines = [UsersInline]
+    list_display = ('project_name', 'project_description', 'project_authors', 'project_date_created', 'project_date_modified')
 
 
 admin.site.register(Project, ProjectAdmin)
