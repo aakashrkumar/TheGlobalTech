3f2fb5117f7a8f0d5d50b91aabc810e1940403be
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 18:36:29 -0800

    Update

--- EnvironmentML/static/js/index.js
+++ EnvironmentML/static/js/index.js
@@ -1,11 +1,3 @@
 function isImage(filename) {
     return ["png", "jpg", "jpeg", "svg"].includes(filename.split('.').pop());
 }
-
-function fadeInPage() {
-    if (!window.AnimationEvent) {
-        return;
-    }
-    var fader = document.getElementById('fader');
-    fader.classList.add('fade-out');
-}
