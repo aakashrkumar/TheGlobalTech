ad8a278c33d303b1fe4b92c4ce260cc723694303
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 18:41:08 -0800

    Update

---
+++ EnvironmentML/static/js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+$('body').css('display', 'none');
+
+$('body').fadeIn(1000);
+
+
+
+$('.link').click(function() {
+
+event.preventDefault();
+
+newLocation = this.href;
+
+$('body').fadeOut(1000, newpage);
+
+});
+
+function newpage() {
+
+window.location = newLocation;
+
+}
+
+});
