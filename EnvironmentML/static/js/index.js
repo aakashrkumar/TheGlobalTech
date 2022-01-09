4e815539b61cef3bf0a2858ed1ebf9e9a5f8eeeb
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 18:32:25 -0800

    Update

--- EnvironmentML/static/js/index.js
+++ EnvironmentML/static/js/index.js
@@ -3,38 +3,43 @@ function isImage(filename) {
 }
 
 function fadeInPage() {
-    if (!window.AnimationEvent) { return; }
+    if (!window.AnimationEvent) {
+        return;
+    }
     var fader = document.getElementById('fader');
     fader.classList.add('fade-out');
 }
-document.addEventListener('DOMContentLoaded', function() {
-    if (!window.AnimationEvent) { return; }
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (!window.AnimationEvent) {
+        return;
+    }
     var anchors = document.getElementsByTagName('a');
 
-    for (var idx=0; idx<anchors.length; idx+=1) {
+    for (var idx = 0; idx < anchors.length; idx += 1) {
         if (anchors[idx].hostname !== window.location.hostname ||
             anchors[idx].pathname === window.location.pathname) {
             continue;
         }
-        anchors[idx].addEventListener('click', function(event) {
+        anchors[idx].addEventListener('click', function (event) {
             var fader = document.getElementById('fader'),
                 anchor = event.currentTarget;
 
-            var listener = function() {
+            var listener = function () {
                 window.location = anchor.href;
                 fader.removeEventListener('animationend', listener);
             };
             fader.addEventListener('animationend', listener);
 
             event.preventDefault();
-            fader.classList.add('fade-in');
+            fader.setAttribute('class', 'fade-in');
         });
     }
 });
 window.addEventListener('pageshow', function (event) {
-  if (!event.persisted) {
-    return;
-  }
-  var fader = document.getElementById('fader');
-  fader.classList.remove('fade-in');
+    if (!event.persisted) {
+        return;
+    }
+    var fader = document.getElementById('fader');
+    fader.setAttribute('class', 'fade-out');
 });
