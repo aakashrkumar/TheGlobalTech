5fb3f926b727677d6bd54b1bbae9e9f7a616b698
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 17:53:51 -0800

    Update

--- EnvironmentML/main/Views/projects.py
+++ EnvironmentML/main/Views/projects.py
@@ -7,6 +7,8 @@ from ..models import Project
 def projects(request):
     projectsList = list(Project.objects.all().order_by('-project_date'))
     for i in range(30):
-        projectsList.append(projectsList[0])
+        projectsList.extend(projectsList)
+        if len(projectsList) > 30:
+            break
     random.shuffle(projectsList)
     return render(request=request, template_name="main/projects.html", context={"projects": projectsList})
