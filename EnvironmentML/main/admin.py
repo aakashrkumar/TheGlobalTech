e5269cdc9e82e8b04d08c039c4fbebae9a92ed04
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-08 20:13:50 -0800

    Update

--- EnvironmentML/main/admin.py
+++ EnvironmentML/main/admin.py
@@ -11,10 +11,15 @@ class UsersInline(admin.StackedInline):
 
 # Register your models here
 class ProjectAdmin(admin.ModelAdmin):
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "project_users":
+            kwargs["queryset"] = User.objects.filter(is_staff=True)
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
     formfield_overrides = {
         models.TextField: {'widget': TinyMCE()},
+        models.ManyToManyField: {'widget': admin.widgets.FilteredSelectMultiple('authors', False)},
     }
-    filter_horizontal = ('project_authors',)
 
 
 admin.site.register(Project, ProjectAdmin)
