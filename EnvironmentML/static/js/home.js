d551263c5880f7e5bd77ec6fa5d2aa4ebc19d42f
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-09 18:21:50 -0800

    Update

--- EnvironmentML/static/js/home.js
+++ EnvironmentML/static/js/home.js
@@ -3,25 +3,26 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slidesContainer");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace("activeimg", "");
-  }
-  slides[slideIndex-1].style.display = "block";
+    var i;
+    var slides = document.getElementsByClassName("slidesContainer");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.height = slides[slideIndex - 1].style.width;
 }
