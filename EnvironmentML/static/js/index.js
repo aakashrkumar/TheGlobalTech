1115626d3954787caab314fd765fd6d008e371e3
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-09 16:44:08 -0800

    Update

--- EnvironmentML/static/js/index.js
+++ EnvironmentML/static/js/index.js
@@ -40,13 +40,9 @@ window.addEventListener('pageshow', function (event) {
     fader.classList.remove('fade-in');
 });
 
-function scrollbarWidth() {
-    var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div>');
-    // Append our div, do our calculation and then remove it
-    $('body').append(div);
-    var w1 = $('div', div).innerWidth();
-    div.css('overflow-y', 'scroll');
-    var w2 = $('div', div).innerWidth();
-    $(div).remove();
-    return (w1 - w2);
-}
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
