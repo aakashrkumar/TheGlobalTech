0b16b87b628a011706b859f46fd5c604a37af35a
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-10 21:46:30 -0800

    Update

--- EnvironmentML/static/js/home.js
+++ EnvironmentML/static/js/home.js
@@ -24,6 +24,7 @@ function showSlides(n) {
         slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].children[0].style.height = $(".project-card:visible").outerWidth() + "px";
     // if height is not equal to outer width, keep trying until it is and print error every time it is not
     // console log outer width
     if ($(window).width() < 600) {
@@ -38,6 +39,7 @@ function showSlides(n) {
 
 // on resize
 $(window).resize(function () {
+    $(".slidesContainer").children().css("height", $(".project-card:visible").outerWidth() + "px");
     // get div by the id home-aligner and remove the class align-center valign-wrapper if screen size is less than 600px
     if ($(window).width() < 600) {
         $("#home-aligner").removeClass("valign-wrapper");
