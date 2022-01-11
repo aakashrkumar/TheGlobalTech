42e851fc3c0f7a910eef1eadebd5f6dd77a4ddb6
Aakash Kumar <aakashinthesky@gmail.com>
2022-01-11 07:44:59 -0800

    Update

--- EnvironmentML/static/js/home.js
+++ EnvironmentML/static/js/home.js
@@ -29,10 +29,20 @@ function showSlides(n) {
     // console log outer width
     if ($(window).width() < 1000) {
         $("#home-aligner").removeClass("valign-wrapper");
-        $("#headline").removeClass("m5");
+        $("#headline").removeClass("m6");
+        $("#headline").addClass("m12");
+
+        $("#slides").removeClass("m6");
+        $("#slides").addClass("m12");
+
     }
     // add the class align-center valign-wrapper if screen size is more than 600px
     else {
+        $("#headline").removeClass("m12");
+        $("#headline").addClass("m6");
+
+        $("#slides").removeClass("12");
+        $("#slides").addClass("m6");
         $("#home-aligner").addClass("valign-wrapper");
     }
 
@@ -44,9 +54,20 @@ $(window).resize(function () {
     // get div by the id home-aligner and remove the class align-center valign-wrapper if screen size is less than 600px
     if ($(window).width() < 1000) {
         $("#home-aligner").removeClass("valign-wrapper");
+        $("#headline").removeClass("m6");
+        $("#headline").addClass("m12");
+
+        $("#slides").removeClass("m6");
+        $("#slides").addClass("m12");
     }
     // add the class align-center valign-wrapper if screen size is more than 600px
     else {
         $("#home-aligner").addClass("valign-wrapper");
+
+        $("#headline").removeClass("m12");
+        $("#headline").addClass("m6");
+
+        $("#slides").removeClass("12");
+        $("#slides").addClass("m6");
     }
 });
