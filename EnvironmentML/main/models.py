3db33427e6e057b568cfa35c29a973028ecf117f
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-05 14:09:12 -0800

    Update

--- EnvironmentML/main/models.py
+++ EnvironmentML/main/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.search import index
+
 
 # Create your models here.
 
@@ -42,3 +49,40 @@ class Volunteers(models.Model):
     # plural name of the model
     class Meta:
         verbose_name_plural = "Volunteers"
+
+
+class ArticlePage(Page):
+    # Database fields
+    body = RichTextField()
+    date = models.DateField("Post date")
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    # Search index configuration
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.FilterField('date'),
+    ]
+
+    # Editor panels configuration
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('body', classname="full"),
+        InlinePanel('related_links', label="Related links"),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        FieldPanel('feed_image'),
+    ]
+
+    # Parent page / subpage type rules
+    parent_page_types = ['article.ArticleIndex']
+    subpage_types = []
