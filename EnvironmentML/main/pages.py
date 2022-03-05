c79bdcc3d45b65e6cee17258f514917abdefd65f
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-05 14:53:25 -0800

    Update

---
+++ EnvironmentML/main/pages.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+
+
+class ArticlePage(Page):
+    # Database fields
+
+    body = RichTextField()
+    date = models.DateField("Post date")
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    # Search index configuration
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+        index.FilterField('date'),
+    ]
+
+    # Editor panels configuration
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('body', classname="full"),
+        InlinePanel('related_links', label="Related links"),
+    ]
+
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Common page configuration"),
+        ImageChooserPanel('feed_image'),
+    ]
+
+
+class ArticlePageRelatedLink(Orderable):
+    page = ParentalKey(ArticlePage, on_delete=models.CASCADE, related_name='related_links')
+    name = models.CharField(max_length=255)
+    url = models.URLField()
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('url'),
+    ]
+
+
+def load():
+    pass
