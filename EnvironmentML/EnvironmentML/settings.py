9ebc90d009aedd922232694970c7cd61a654bc17
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-05 12:18:26 -0800

    Update

--- EnvironmentML/EnvironmentML/settings.py
+++ EnvironmentML/EnvironmentML/settings.py
@@ -129,19 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_ROOT = "media/"
 STATICFILES_DIRS = []
 
-# TINYMCE
-X_FRAME_OPTIONS = 'SAMEORIGIN'
-TINYMCE_DEFAULT_CONFIG = {
-    "height": "320px",
-    "width": "960px",
-    "menubar": "file edit view insert format tools table help",
-    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
-               "fullscreen insertdatetime media table paste code help wordcount",
-    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
-               "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
-               "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
-               "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
-               "a11ycheck ltr rtl | showcomments addcomment code",
-    "custom_undo_redo_levels": 10,
-}
-TINYMCE_FILEBROWSER = True
+TINYMCE_JS_URL = 'https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js'
+TINYMCE_COMPRESSOR = False
