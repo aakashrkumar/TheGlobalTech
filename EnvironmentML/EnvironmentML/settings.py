bc974d1134d29427b44200b1e05e887d0126cb80
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-05 12:58:41 -0800

    Update

--- EnvironmentML/EnvironmentML/settings.py
+++ EnvironmentML/EnvironmentML/settings.py
@@ -40,22 +40,7 @@ INSTALLED_APPS = [
     'filebrowser',
     "tinymce",
     "jquery",
-    # wagtail
-    'wagtail.contrib.forms',
-    'wagtail.contrib.redirects',
-    'wagtail.embeds',
-    'wagtail.sites',
-    'wagtail.users',
-    'wagtail.snippets',
-    'wagtail.documents',
-    'wagtail.images',
-    'wagtail.search',
-    'wagtail.admin',
-    'wagtail.core',
 
-    'modelcluster',
-    'taggit',
-    # end wagtail
     'main.apps.MainConfig',
 ]
 
@@ -67,8 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # wagtail
-    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
 ROOT_URLCONF = 'EnvironmentML.urls'
