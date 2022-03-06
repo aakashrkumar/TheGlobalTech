e9c5c1672d1c91eaa4552c0167d346655597b73c
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-05 16:51:09 -0800

    Update

--- EnvironmentML/EnvironmentML/urls.py
+++ EnvironmentML/EnvironmentML/urls.py
@@ -19,6 +19,7 @@ from filebrowser.sites import site
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.images import urls as wagtailimages_urls
 from puput import urls as puput_urls
 
 site.directory = "uploads/"
@@ -32,6 +33,7 @@ urlpatterns = [
 
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
+    path('images/', include(wagtailimages_urls)),
     path(r'', include(puput_urls)),
     path('', include(wagtail_urls)),
 ]
