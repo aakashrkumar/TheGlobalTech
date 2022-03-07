e50323a7d6e0362b7a705ea5974bd5bf058a39c0
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-07 14:45:50 -0800

    Update

--- EnvironmentML/main/widgets.py
+++ EnvironmentML/main/widgets.py
@@ -4,7 +4,5 @@ from django_select2 import forms as s2forms
 class AuthorWidget(s2forms.ModelSelect2Widget):
     search_fields = [
         "username__icontains",
-        "first_name__icontains",
-        "last_name__icontains",
         "email__icontains",
     ]
