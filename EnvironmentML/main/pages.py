bc5830284ead74f96a6561188eb06da28cf294f9
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-07 14:55:03 -0800

    Update

--- EnvironmentML/main/pages.py
+++ EnvironmentML/main/pages.py
@@ -96,7 +96,7 @@ class ProjectPage(Page, ContentImportMixin):
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
-        InlinePanel('project_authors', widget=AuthorsWidget),
+        InlinePanel('project_authors'),
         ImageChooserPanel('project_image'),
         FieldPanel('body', classname="full"),
         InlinePanel('related_links', label="Related links"),
