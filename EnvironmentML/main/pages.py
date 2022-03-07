278035002df17ee8f4bef2b0d7ce0a259b621ff6
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-07 15:26:13 -0800

    Update

--- EnvironmentML/main/pages.py
+++ EnvironmentML/main/pages.py
@@ -112,8 +112,8 @@ class ProjectPage(Page, ContentImportMixin):
 
 
 class Authors(Orderable):
-    page = ParentalKey(ProjectPage, on_delete=models.CASCADE, related_name='authors')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    page = ParentalKey(ProjectPage, on_delete=models.SET_NULL, related_name='authors')
+    user = models.ForeignKey(User, on_delete=models.SET_NULL)
 
     panels = [
         FieldPanel('user'),
