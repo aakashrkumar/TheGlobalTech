fa11ee9060c7d0f99d3961e2d44204ebf7bf9005
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-07 14:56:51 -0800

    Update

--- EnvironmentML/main/pages.py
+++ EnvironmentML/main/pages.py
@@ -66,7 +66,7 @@ class AboutUSPage(Page):
 class ProjectPage(Page, ContentImportMixin):
     # Database fields
     mapper_class = MyMapper
-    project_authors = ParentalManyToManyField('django.User')
+    project_authors = ParentalManyToManyField('django.User', related_name='project_authors')
     date = models.DateField("Post date")
 
     body = StreamField([
