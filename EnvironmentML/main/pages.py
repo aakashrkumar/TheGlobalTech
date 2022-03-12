ab39b1437558bb4da971a5ab0d307775fc49dcff
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-12 13:29:22 -0800

    Update

--- EnvironmentML/main/pages.py
+++ EnvironmentML/main/pages.py
@@ -91,8 +91,11 @@ class ProjectPage(Page, ContentImportMixin):
         ('heading', CharBlock()),
         ('table', TableBlock()),
         ('paragraph',
-         RichTextBlock(classname="full", features=['h1', 'h2', 'h3', 'h4', 'h5', 'code', 'bold', 'italic', 'link', 'ol', 'ul', 'hr', 'document-link', 'image', 'embed'])),
-    ])
+         RichTextBlock(classname="full",
+                       features=['h1', 'h2', 'h3', 'h4', 'h5', 'code', 'bold', 'italic', 'link', 'ol', 'ul', 'hr', 'document-link', 'image', 'embed', 'blockquote',
+                                 'superscript', 'subscript', 'strikethrough', 'preformatted', 'clean', 'full-width', 'align-left', 'align-center', 'align-right']),
+         )]
+    )
 
     project_image = models.ForeignKey(
         'wagtailimages.Image',
