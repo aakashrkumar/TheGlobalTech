c99d2cbdafc36db234b5f4a0e46731953028ee84
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-12 13:48:47 -0800

    Update

--- EnvironmentML/main/pages.py
+++ EnvironmentML/main/pages.py
@@ -96,7 +96,7 @@ class ProjectPage(Page, ContentImportMixin):
                        features=['h1', 'h2', 'h3', 'h4', 'h5', 'code', 'bold', 'italic', 'link', 'ol', 'ul', 'hr', 'document-link', 'image', 'embed', 'blockquote',
                                  'superscript', 'subscript', 'strikethrough']),
          ),
-        ('embed', EmbedBlock(max_width=2560, max_height=1920, width="100%")),
+        ('embed', EmbedBlock(width="100%", max_width="100%")),
     ])
 
     project_image = models.ForeignKey(
