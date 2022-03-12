b45db5f0d5f66d3ee1e9aefd0fdf04e48720fc65
Aakash Kumar <aakashinthesky@gmail.com>
2022-03-12 13:29:44 -0800

    Update

--- EnvironmentML/main/pages.py
+++ EnvironmentML/main/pages.py
@@ -93,7 +93,7 @@ class ProjectPage(Page, ContentImportMixin):
         ('paragraph',
          RichTextBlock(classname="full",
                        features=['h1', 'h2', 'h3', 'h4', 'h5', 'code', 'bold', 'italic', 'link', 'ol', 'ul', 'hr', 'document-link', 'image', 'embed', 'blockquote',
-                                 'superscript', 'subscript', 'strikethrough', 'preformatted', 'clean', 'full-width', 'align-left', 'align-center', 'align-right']),
+                                 'superscript', 'subscript', 'strikethrough']),
          )]
     )
 
